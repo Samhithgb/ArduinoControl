0541aba1f25d183e983387e594180c3fa9b37ae0
Samhithgb <gbsamhith@gmail.com>
2016-04-06 12:15:21 +0530

    added php scripts

---
+++ sketch_oct15a.ino
@@ -0,0 +1,26 @@
+int ledPin=13;
+int state=0;
+int data=0;
+ 
+void setup()
+{
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,state);
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+  Serial.println(data);
+  if(Serial.available()>0)
+  {
+    state=!state;
+    data=Serial.read();
+    switch(data)
+    {
+      case 177:
+        digitalWrite(ledPin,state);
+      break;
+    }
+  }
+}
